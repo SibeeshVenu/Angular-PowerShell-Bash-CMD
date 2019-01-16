8139a06cd94d3f390cd56eeb8b3cb77fbb81b1d6
Sibeesh Venu <sibeesh.venu@gmail.com>
2019-01-16 15:58:52 +0100

    Update server.js

--- node/server.js
+++ node/server.js
@@ -1,14 +1,25 @@
 var express = require('express');
+var azure = require('azure-storage');
+var config = require('./config.json');
+var cors = require('cors');
+const shell = require('node-powershell');
+
 var app = express();
+app.use(cors());
 app.listen(8080);
-app.get('/version', function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  var spawn = require("child_process").spawn;
-  var child = spawn("powershell.exe", ["-Command", req.query.command]);
-  child.stdout.on("data", (data) => {
-    res.send(data);
-    child.stdin.end();
+
+app.get('/version', (req, res) => {
+  let ps = new shell({
+    executionPolicy: 'Bypass',
+    noProfile: true
   });
+  ps.addCommand(req.query.command);
+  ps.invoke()
+    .then(output => {
+      res.send(output);
+    })
+    .catch(err => {
+      res.send(err);
+      ps.dispose();
+    });
 });
