826a6682b3413da9c6c1eb5980fd7804b094b003
Sibeesh Venu <sibeesh.venu@gmail.com>
2019-01-16 11:34:56 +0100

    Get the command output without plugin

--- node/server.js
+++ node/server.js
@@ -1,25 +1,14 @@
 var express = require('express');
-const shell = require('node-powershell');
 var app = express();
 app.listen(8080);
-app.get('/', function () {
-  console.log('Server started');
-});
-
 app.get('/version', function (req, res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  let ps = new shell({
-    executionPolicy: 'Bypass',
-    noProfile: true
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  var spawn = require("child_process").spawn;
+  var child = spawn("powershell.exe", ["-Command", req.query.command]);
+  child.stdout.on("data", (data) => {
+    res.send(data);
+    child.stdin.end();
   });
-  ps.addCommand(req.query.command);
-  ps.invoke()
-    .then(output => {
-      res.send(output);
-    })
-    .catch(err => {
-      res.send(err);
-      ps.dispose();
-    });
 });
